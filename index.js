bf96853ca178f4a47ef72965a62e728e04fad5f7
Marvin Thompson <marvinthompson@pursuit.org>
2019-12-17 21:37:52 -0500

    add css

--- index.js
+++ index.js
@@ -6,20 +6,36 @@ document.addEventListener("DOMContentLoaded", () => {
     let select = document.querySelector("select");
     let submit = document.querySelector("#submit");
     let content = document.querySelector(".content");
-    let limit = 10;
     let search = "cats"
 
 
     let h1 = document.createElement("h1");
-    form.addEventListener("submit", () => {
-        searchInput(userInput, limit)
+
+    const showGifs = (arr) => { 
+        arr.forEach((gif) => {
+            let image = document.createElement("img")
+            image.src = gif.images.downsized.url
+            content.appendChild(image)
+        }) 
+    }
+    
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        searchInput(userInput.value, select.value);
     })
+    
+    select.addEventListener("change", (e) => {
+        select.value = e.target.value
+    })
+    
     const searchInput = async (userInput, limit) => {
         try {
-            let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}&limit=${limit}`)
-            console.log(res)
+            content.innerHTML = ""
+            let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}&limit=${limit}`) 
+            let imageArr = res.data.data
+            showGifs(imageArr)
         } catch (error) {
-            debugger   
+            console.log(error)
         }
     }
 
